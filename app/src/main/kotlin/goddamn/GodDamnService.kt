1fc27ad74f4050345e580a2bce385147875c046e
Elvis Porebski <eporebski@zendesk.com>
2019-03-02 20:40:25 +0000

    Nearby god damns
    Share your god damn with like-minded people nearby

--- app/src/main/kotlin/goddamn/GodDamnService.kt
+++ app/src/main/kotlin/goddamn/GodDamnService.kt
@@ -1,6 +1,5 @@
-package com.mbcdev.goddamn
+package goddamn
 
-import android.media.MediaPlayer
 import android.os.Build
 import android.service.quicksettings.TileService
 import android.support.annotation.RequiresApi
@@ -15,15 +14,7 @@ import android.support.annotation.RequiresApi
 @RequiresApi(api = Build.VERSION_CODES.N)
 class GodDamnService : TileService() {
 
-    val mediaPlayer by lazy {
-        MediaPlayer.create(this@GodDamnService, R.raw.goddamn)
-    }
-
     override fun onClick() {
-        if (mediaPlayer.isPlaying) {
-            mediaPlayer.seekTo(0)
-        }
-
-        mediaPlayer.start()
+        GodDamnApp.get(this).godDamn()
     }
 }
